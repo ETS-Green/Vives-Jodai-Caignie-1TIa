7233412e47774581f93d53d8f61aa5e17dc6c4de
Jodai Caignie <jodai.caignie@outlook.com>
2022-03-14 13:00:58 +0100

    labo 3 afgewerkt

--- semester_2/labo_3/js/opdracht_3.js
+++ semester_2/labo_3/js/opdracht_3.js
@@ -1,18 +1,21 @@
 "use strict";
 
 const Calculate = () => {
+    let totaal = 0.0;
     for (let i = 1; i < 4; i++) {
-        let prijs = document.querySelector(`#product${i} td:nth-child(2)`).innerText;
-        let aantal = document.querySelector(`#product${i} td:nth-child(3)`).innerText;
-        let btw = document.querySelector(`#product${i} td:nth-child(4)`).innerText;
-        let subtot = document.querySelector(`#product${i} td:nth-child(5)`).innerText;
+        let prijs   = document.querySelector(`#product${i} td:nth-child(2)`).innerText;
+        let aantal  = document.querySelector(`#product${i} td:nth-child(3) input`).value;
+        let btw     = document.querySelector(`#product${i} td:nth-child(4)`).innerText;
+        let subtot  = document.querySelector(`#product${i} td:nth-child(5)`);
 
-        prijs = parseFloat(prijs.substring(1,3));
+        prijs = parseFloat(prijs);
         aantal = parseInt(aantal, 10);
-        btw = parseInt(aantal, 10)/100;
-
-        subtot = prijs * aantal + (prijs * aantal * btw);
+        btw = parseFloat(btw)/100;
+        let result = prijs * aantal + (prijs * aantal * btw);
+        totaal += result;
+        subtot.innerText = Number(result).toFixed(2) + " Eur";
     }
+    document.getElementById("totaal").innerText = Number(totaal).toFixed(2) + " Eur";
 }
 
 const setup = () => {
